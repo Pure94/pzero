4498142bdd105a5ac3438d71b16d70cac37dc8af
Kamil <siepkakamil@gmail.com>
2022-03-17 17:43:24 +0100

    Init

---
+++ src/main/java/com/example/pzero/PzeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.pzero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PzeroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PzeroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pzero/PzeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pzero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PzeroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
