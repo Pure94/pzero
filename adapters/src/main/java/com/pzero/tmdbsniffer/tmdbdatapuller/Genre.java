b7516ff53192914d70c71d03d77ea9398a00f76b
Kamil <siepkakamil@gmail.com>
2022-03-28 20:29:49 +0200

    fix some typos, (#6)
    rm liquidbase
    add postgre-dockerfile for tables init

--- adapters/src/main/java/com/pzero/tmdbsniffer/tmdbdatapuller/Genre.java
+++ adapters/src/main/java/com/pzero/tmdbsniffer/tmdbdatapuller/Genre.java
@@ -1,25 +1,26 @@
 package com.pzero.tmdbsniffer.tmdbdatapuller;
 
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 import utils.models.AbstractEntity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Getter
 @Setter
 @Table(name = "genres")
-
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
 class Genre extends AbstractEntity {
 
     @EqualsAndHashCode.Include
+    @Column(name = "tmdb_genre_id", nullable = false)
     private Integer tmdbId;
 
     @EqualsAndHashCode.Include
+    @Column(name = "name", nullable = false)
     private String name;
 }
